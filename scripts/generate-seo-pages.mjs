1978df12ec540f7d1bb2a10db499b2108dec0892
trippielv92-Arch <trippielv92@gmail.com>
2026-09-22 11:37:34 +0300

    Add Kenya county SEO landing pages

--- scripts/generate-seo-pages.mjs
+++ scripts/generate-seo-pages.mjs
@@ -113,6 +113,339 @@ const staticRoutes = [
   "billing"
 ];
 
+const locations = [
+  {
+    slug: "nairobi",
+    name: "Nairobi",
+    intro:
+      "Explore apartments and rental homes in Nairobi, Kenya. Compare available homes by location, bedrooms and monthly rent, then connect directly with property owners through JUMAA.",
+    properties: properties.filter(property => property.county === "Nairobi")
+  },
+  {
+    slug: "mombasa",
+    name: "Mombasa",
+    intro:
+      "Explore apartments and rental homes in Mombasa, Kenya with JUMAA. Check available properties and discover homes in Mombasa as listings become available.",
+    properties: properties.filter(property => property.county === "Mombasa")
+  },
+  {
+    slug: "kwale",
+    name: "Kwale",
+    intro:
+      "Explore apartments and rental homes in Kwale, Kenya with JUMAA. Check available properties and discover homes in Kwale as listings become available.",
+    properties: properties.filter(property => property.county === "Kwale")
+  },
+  {
+    slug: "kilifi",
+    name: "Kilifi",
+    intro:
+      "Explore apartments and rental homes in Kilifi, Kenya with JUMAA. Check available properties and discover homes in Kilifi as listings become available.",
+    properties: properties.filter(property => property.county === "Kilifi")
+  },
+  {
+    slug: "tana-river",
+    name: "Tana River",
+    intro:
+      "Explore apartments and rental homes in Tana River, Kenya with JUMAA. Check available properties and discover homes in Tana River as listings become available.",
+    properties: properties.filter(property => property.county === "Tana River")
+  },
+  {
+    slug: "lamu",
+    name: "Lamu",
+    intro:
+      "Explore apartments and rental homes in Lamu, Kenya with JUMAA. Check available properties and discover homes in Lamu as listings become available.",
+    properties: properties.filter(property => property.county === "Lamu")
+  },
+  {
+    slug: "taita-taveta",
+    name: "Taita-Taveta",
+    intro:
+      "Explore apartments and rental homes in Taita-Taveta, Kenya with JUMAA. Check available properties and discover homes in Taita-Taveta as listings become available.",
+    properties: properties.filter(property => property.county === "Taita-Taveta")
+  },
+  {
+    slug: "garissa",
+    name: "Garissa",
+    intro:
+      "Explore apartments and rental homes in Garissa, Kenya with JUMAA. Check available properties and discover homes in Garissa as listings become available.",
+    properties: properties.filter(property => property.county === "Garissa")
+  },
+  {
+    slug: "wajir",
+    name: "Wajir",
+    intro:
+      "Explore apartments and rental homes in Wajir, Kenya with JUMAA. Check available properties and discover homes in Wajir as listings become available.",
+    properties: properties.filter(property => property.county === "Wajir")
+  },
+  {
+    slug: "mandera",
+    name: "Mandera",
+    intro:
+      "Explore apartments and rental homes in Mandera, Kenya with JUMAA. Check available properties and discover homes in Mandera as listings become available.",
+    properties: properties.filter(property => property.county === "Mandera")
+  },
+  {
+    slug: "marsabit",
+    name: "Marsabit",
+    intro:
+      "Explore apartments and rental homes in Marsabit, Kenya with JUMAA. Check available properties and discover homes in Marsabit as listings become available.",
+    properties: properties.filter(property => property.county === "Marsabit")
+  },
+  {
+    slug: "isiolo",
+    name: "Isiolo",
+    intro:
+      "Explore apartments and rental homes in Isiolo, Kenya with JUMAA. Check available properties and discover homes in Isiolo as listings become available.",
+    properties: properties.filter(property => property.county === "Isiolo")
+  },
+  {
+    slug: "meru",
+    name: "Meru",
+    intro:
+      "Explore apartments and rental homes in Meru, Kenya with JUMAA. Check available properties and discover homes in Meru as listings become available.",
+    properties: properties.filter(property => property.county === "Meru")
+  },
+  {
+    slug: "tharaka-nithi",
+    name: "Tharaka-Nithi",
+    intro:
+      "Explore apartments and rental homes in Tharaka-Nithi, Kenya with JUMAA. Check available properties and discover homes in Tharaka-Nithi as listings become available.",
+    properties: properties.filter(property => property.county === "Tharaka-Nithi")
+  },
+  {
+    slug: "embu",
+    name: "Embu",
+    intro:
+      "Explore apartments and rental homes in Embu, Kenya with JUMAA. Check available properties and discover homes in Embu as listings become available.",
+    properties: properties.filter(property => property.county === "Embu")
+  },
+  {
+    slug: "kitui",
+    name: "Kitui",
+    intro:
+      "Explore apartments and rental homes in Kitui, Kenya with JUMAA. Check available properties and discover homes in Kitui as listings become available.",
+    properties: properties.filter(property => property.county === "Kitui")
+  },
+  {
+    slug: "machakos",
+    name: "Machakos",
+    intro:
+      "Explore apartments and rental homes in Machakos, Kenya with JUMAA. Check available properties and discover homes in Machakos as listings become available.",
+    properties: properties.filter(property => property.county === "Machakos")
+  },
+  {
+    slug: "makueni",
+    name: "Makueni",
+    intro:
+      "Explore apartments and rental homes in Makueni, Kenya with JUMAA. Check available properties and discover homes in Makueni as listings become available.",
+    properties: properties.filter(property => property.county === "Makueni")
+  },
+  {
+    slug: "nyandarua",
+    name: "Nyandarua",
+    intro:
+      "Explore apartments and rental homes in Nyandarua, Kenya with JUMAA. Check available properties and discover homes in Nyandarua as listings become available.",
+    properties: properties.filter(property => property.county === "Nyandarua")
+  },
+  {
+    slug: "nyeri",
+    name: "Nyeri",
+    intro:
+      "Explore apartments and rental homes in Nyeri, Kenya with JUMAA. Check available properties and discover homes in Nyeri as listings become available.",
+    properties: properties.filter(property => property.county === "Nyeri")
+  },
+  {
+    slug: "kirinyaga",
+    name: "Kirinyaga",
+    intro:
+      "Explore apartments and rental homes in Kirinyaga, Kenya with JUMAA. Check available properties and discover homes in Kirinyaga as listings become available.",
+    properties: properties.filter(property => property.county === "Kirinyaga")
+  },
+  {
+    slug: "murang'a",
+    name: "Murang'a",
+    intro:
+      "Explore apartments and rental homes in Murang'a, Kenya with JUMAA. Check available properties and discover homes in Murang'a as listings become available.",
+    properties: properties.filter(property => property.county === "Murang'a")
+  },
+  {
+    slug: "kiambu",
+    name: "Kiambu",
+    intro:
+      "Explore apartments and rental homes in Kiambu, Kenya with JUMAA. Check available properties and discover homes in Kiambu as listings become available.",
+    properties: properties.filter(property => property.county === "Kiambu")
+  },
+  {
+    slug: "turkana",
+    name: "Turkana",
+    intro:
+      "Explore apartments and rental homes in Turkana, Kenya with JUMAA. Check available properties and discover homes in Turkana as listings become available.",
+    properties: properties.filter(property => property.county === "Turkana")
+  },
+  {
+    slug: "west-pokot",
+    name: "West Pokot",
+    intro:
+      "Explore apartments and rental homes in West Pokot, Kenya with JUMAA. Check available properties and discover homes in West Pokot as listings become available.",
+    properties: properties.filter(property => property.county === "West Pokot")
+  },
+  {
+    slug: "samburu",
+    name: "Samburu",
+    intro:
+      "Explore apartments and rental homes in Samburu, Kenya with JUMAA. Check available properties and discover homes in Samburu as listings become available.",
+    properties: properties.filter(property => property.county === "Samburu")
+  },
+  {
+    slug: "trans-nzoia",
+    name: "Trans Nzoia",
+    intro:
+      "Explore apartments and rental homes in Trans Nzoia, Kenya with JUMAA. Check available properties and discover homes in Trans Nzoia as listings become available.",
+    properties: properties.filter(property => property.county === "Trans Nzoia")
+  },
+  {
+    slug: "uasin-gishu",
+    name: "Uasin Gishu",
+    intro:
+      "Explore apartments and rental homes in Uasin Gishu, Kenya with JUMAA. Check available properties and discover homes in Uasin Gishu as listings become available.",
+    properties: properties.filter(property => property.county === "Uasin Gishu")
+  },
+  {
+    slug: "elgeyo-marakwet",
+    name: "Elgeyo-Marakwet",
+    intro:
+      "Explore apartments and rental homes in Elgeyo-Marakwet, Kenya with JUMAA. Check available properties and discover homes in Elgeyo-Marakwet as listings become available.",
+    properties: properties.filter(property => property.county === "Elgeyo-Marakwet")
+  },
+  {
+    slug: "nandi",
+    name: "Nandi",
+    intro:
+      "Explore apartments and rental homes in Nandi, Kenya with JUMAA. Check available properties and discover homes in Nandi as listings become available.",
+    properties: properties.filter(property => property.county === "Nandi")
+  },
+  {
+    slug: "baringo",
+    name: "Baringo",
+    intro:
+      "Explore apartments and rental homes in Baringo, Kenya with JUMAA. Check available properties and discover homes in Baringo as listings become available.",
+    properties: properties.filter(property => property.county === "Baringo")
+  },
+  {
+    slug: "laikipia",
+    name: "Laikipia",
+    intro:
+      "Explore apartments and rental homes in Laikipia, Kenya with JUMAA. Check available properties and discover homes in Laikipia as listings become available.",
+    properties: properties.filter(property => property.county === "Laikipia")
+  },
+  {
+    slug: "nakuru",
+    name: "Nakuru",
+    intro:
+      "Find apartments and rental homes in Nakuru, Kenya. Explore available properties in areas such as Nakuru Town and Milimani and connect directly with property owners through JUMAA.",
+    properties: properties.filter(property => property.county === "Nakuru")
+  },
+  {
+    slug: "narok",
+    name: "Narok",
+    intro:
+      "Explore apartments and rental homes in Narok, Kenya with JUMAA. Check available properties and discover homes in Narok as listings become available.",
+    properties: properties.filter(property => property.county === "Narok")
+  },
+  {
+    slug: "kajiado",
+    name: "Kajiado",
+    intro:
+      "Explore apartments and rental homes in Kajiado, Kenya with JUMAA. Check available properties and discover homes in Kajiado as listings become available.",
+    properties: properties.filter(property => property.county === "Kajiado")
+  },
+  {
+    slug: "kericho",
+    name: "Kericho",
+    intro:
+      "Explore apartments and rental homes in Kericho, Kenya. Find available homes near Kericho town and connect with property owners through JUMAA.",
+    properties: properties.filter(property => property.county === "Kericho")
+  },
+  {
+    slug: "bomet",
+    name: "Bomet",
+    intro:
+      "Find apartments and rental homes in Bomet, Kenya. Explore available homes in Bomet Town and connect directly with property owners through JUMAA.",
+    properties: properties.filter(property => property.county === "Bomet")
+  },
+  {
+    slug: "kakamega",
+    name: "Kakamega",
+    intro:
+      "Explore apartments and rental homes in Kakamega, Kenya with JUMAA. Check available properties and discover homes in Kakamega as listings become available.",
+    properties: properties.filter(property => property.county === "Kakamega")
+  },
+  {
+    slug: "vihiga",
+    name: "Vihiga",
+    intro:
+      "Explore apartments and rental homes in Vihiga, Kenya with JUMAA. Check available properties and discover homes in Vihiga as listings become available.",
+    properties: properties.filter(property => property.county === "Vihiga")
+  },
+  {
+    slug: "bungoma",
+    name: "Bungoma",
+    intro:
+      "Explore apartments and rental homes in Bungoma, Kenya with JUMAA. Check available properties and discover homes in Bungoma as listings become available.",
+    properties: properties.filter(property => property.county === "Bungoma")
+  },
+  {
+    slug: "busia",
+    name: "Busia",
+    intro:
+      "Explore apartments and rental homes in Busia, Kenya with JUMAA. Check available properties and discover homes in Busia as listings become available.",
+    properties: properties.filter(property => property.county === "Busia")
+  },
+  {
+    slug: "siaya",
+    name: "Siaya",
+    intro:
+      "Explore apartments and rental homes in Siaya, Kenya with JUMAA. Check available properties and discover homes in Siaya as listings become available.",
+    properties: properties.filter(property => property.county === "Siaya")
+  },
+  {
+    slug: "kisumu",
+    name: "Kisumu",
+    intro:
+      "Discover apartments and rental homes in Kisumu, Kenya. Explore available homes, compare monthly rents and connect with property owners through JUMAA.",
+    properties: properties.filter(property => property.county === "Kisumu")
+  },
+  {
+    slug: "homa-bay",
+    name: "Homa Bay",
+    intro:
+      "Explore apartments and rental homes in Homa Bay, Kenya with JUMAA. Check available properties and discover homes in Homa Bay as listings become available.",
+    properties: properties.filter(property => property.county === "Homa Bay")
+  },
+  {
+    slug: "migori",
+    name: "Migori",
+    intro:
+      "Explore apartments and rental homes in Migori, Kenya with JUMAA. Check available properties and discover homes in Migori as listings become available.",
+    properties: properties.filter(property => property.county === "Migori")
+  },
+  {
+    slug: "kisii",
+    name: "Kisii",
+    intro:
+      "Explore apartments and rental homes in Kisii, Kenya with JUMAA. Check available properties and discover homes in Kisii as listings become available.",
+    properties: properties.filter(property => property.county === "Kisii")
+  },
+  {
+    slug: "nyamira",
+    name: "Nyamira",
+    intro:
+      "Explore apartments and rental homes in Nyamira, Kenya with JUMAA. Check available properties and discover homes in Nyamira as listings become available.",
+    properties: properties.filter(property => property.county === "Nyamira")
+  }
+];
+
+
 const escapeHtml = value =>
   String(value)
     .replaceAll("&", "&amp;")
@@ -231,6 +564,142 @@ function buildPropertyHtml(property) {
   return html;
 }
 
+function buildLocationHtml(location) {
+  const canonical =
+    `https://jumaaweb.vercel.app/apartments/${location.slug}`;
+
+  const title =
+    `Apartments for Rent in ${location.name}, Kenya | JUMAA`;
+
+  const description =
+    location.intro;
+
+  const schema = {
+    "@context": "https://schema.org",
+    "@type": "CollectionPage",
+    name: title,
+    description,
+    url: canonical,
+    about: {
+      "@type": "Place",
+      name: location.name,
+      address: {
+        "@type": "PostalAddress",
+        addressRegion: location.name,
+        addressCountry: "KE"
+      }
+    },
+    mainEntity: {
+      "@type": "ItemList",
+      numberOfItems: location.properties.length,
+      itemListElement: location.properties.map((property, index) => ({
+        "@type": "ListItem",
+        position: index + 1,
+        name: property.name,
+        url:
+          `https://jumaaweb.vercel.app/property/${property.id}`
+      }))
+    }
+  };
+
+  const cards = location.properties.map(property => `
+    <article>
+      <a href="/property/${property.id}">
+        <img
+          src="${property.image}"
+          alt="${escapeHtml(property.name)}"
+          loading="lazy"
+        />
+        <h2>${escapeHtml(property.name)}</h2>
+      </a>
+      <p>${escapeHtml(property.area)}, ${escapeHtml(property.county)}</p>
+      <p>${escapeHtml(property.house)} from ${formatMoney(property.price)} / month</p>
+      <a href="/property/${property.id}">View property</a>
+    </article>
+  `).join("");
+
+  let html = baseHtml;
+
+  html = html.replace(
+    /<title>[\s\S]*?<\/title>/,
+    `<title>${escapeHtml(title)}</title>`
+  );
+
+  html = html.replace(
+    /<meta\s+name="description"\s+content="[^"]*"\s*\/?>/,
+    `<meta name="description" content="${escapeHtml(description)}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+name="robots"\s+content="[^"]*"\s*\/?>/,
+    `<meta name="robots" content="index, follow" />`
+  );
+
+  html = html.replace(
+    /<link\s+rel="canonical"\s+href="[^"]*"\s*\/?>/,
+    `<link rel="canonical" href="${canonical}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+property="og:title"\s+content="[^"]*"\s*\/?>/,
+    `<meta property="og:title" content="${escapeHtml(title)}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+property="og:description"\s+content="[^"]*"\s*\/?>/,
+    `<meta property="og:description" content="${escapeHtml(description)}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+property="og:url"\s+content="[^"]*"\s*\/?>/,
+    `<meta property="og:url" content="${canonical}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+property="og:image"\s+content="[^"]*"\s*\/?>/,
+    `<meta property="og:image" content="${location.properties[0]?.image || ""}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+name="twitter:title"\s+content="[^"]*"\s*\/?>/,
+    `<meta name="twitter:title" content="${escapeHtml(title)}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+name="twitter:description"\s+content="[^"]*"\s*\/?>/,
+    `<meta name="twitter:description" content="${escapeHtml(description)}" />`
+  );
+
+  html = html.replace(
+    /<meta\s+name="twitter:image"\s+content="[^"]*"\s*\/?>/,
+    `<meta name="twitter:image" content="${location.properties[0]?.image || ""}" />`
+  );
+
+  html = html.replace(
+    "</head>",
+    `<script type="application/ld+json">${JSON.stringify(schema)}</script>\n</head>`
+  );
+
+  html = html.replace(
+    /<body([^>]*)>/,
+    `<body$1>
+      <main style="max-width:1100px;margin:0 auto;padding:40px 20px;">
+        <header>
+          <p>JUMAA — Kenya home search</p>
+          <h1>Apartments for Rent in ${escapeHtml(location.name)}, Kenya</h1>
+          <p>${escapeHtml(location.intro)}</p>
+        </header>
+
+        <section>
+          <h2>Available homes in ${escapeHtml(location.name)}</h2>
+          ${cards || `<p>No properties are currently listed in ${escapeHtml(location.name)}.</p>`}
+        </section>
+      </main>`
+  );
+
+  return html;
+}
+
 function writePage(route, html) {
   const directory = path.join(distDir, route);
 
@@ -256,6 +725,13 @@ for (const property of properties) {
 
 for (const route of staticRoutes) {
   writePage(route, baseHtml);
+}
+
+for (const location of locations) {
+  writePage(
+    `apartments/${location.slug}`,
+    buildLocationHtml(location)
+  );
 }
 
 console.log("\nSEO page generation complete.\n");
